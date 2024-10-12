ee9f860d8432c38e4e762d5f82112bfd0bf42d23
USSMANsarwar1024 <165565408+USSMANsarwar1024@users.noreply.github.com>
2024-10-12 09:04:31 +0000

    modified:   abc.cpp

--- abc.cpp
+++ abc.cpp
@@ -1,7 +1,29 @@
-#include<iostream>
+//Exercise 1
+// Write a program that asks the user to enter integers as inputs to be stored in the variables 'a' and 'b' respectively. 
+// There are also two integer pointers named ptrA and ptrB. 
+// Assign the values of 'a' and 'b' to ptrA and ptrB respectively, and display them.
+
+#include <iostream>
 using namespace std;
 
 int main(void)
 {
-    cout<<"Hello, World\n";
+    int a{0}, b{0};
+
+    cout<<"a : ";
+    cin>>a;
+    cout<<"b : ";
+    cin>>b;
+
+    cout<<"a : "<<a <<" b : "<<b <<endl;
+
+    int *ptra, *ptrb;
+
+    ptra = &a;
+    ptrb = &b;
+
+    cout<<"ptra as assigned as a : "<<ptra<<endl;
+    cout<<"ptrb as assigned as b : "<<ptrb<<endl;
+
+    return 0;
 }
